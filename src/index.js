c27d421fba4fd7fd46d022425be91f3dfc302bf5
Mark <marcelokopmann@gmail.com>
2019-07-16 20:25:19 -0300

    reverted

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
