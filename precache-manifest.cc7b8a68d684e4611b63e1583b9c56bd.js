5f2bda08efd9226eb5db54ee52d8bb3f4bed745d
Mark <marcelokopmann@gmail.com>
2019-07-16 12:19:00 -0300

    Updates

---
+++ precache-manifest.cc7b8a68d684e4611b63e1583b9c56bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db21f417e32f4ee5da35c45e5dd0ae5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e338fca920125aec4b8",
+    "url": "/static/css/main.0bc6acd3.chunk.css"
+  },
+  {
+    "revision": "a8d8a8497937e5b2ba01",
+    "url": "/static/js/2.7376e9da.chunk.js"
+  },
+  {
+    "revision": "0e338fca920125aec4b8",
+    "url": "/static/js/main.daa413bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
