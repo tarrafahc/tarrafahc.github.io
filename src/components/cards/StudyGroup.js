41fc4d1fb5e5464b624232cc671fce600af342fe
Mark Kop <mk.marcelo.mk@gmail.com>
2020-03-01 17:49:40 -0300

    fix: group card and info

--- src/components/cards/StudyGroup.js
+++ src/components/cards/StudyGroup.js
@@ -1,15 +1,25 @@
 import React from 'react'
 
 const StudyGroup = props => {
-  const { title, description, organizer, forBegginers, date, tags } = props.group
+  const { title, description, organizer, forBegginers, date, tags, link } = props.group
   const tagsElements = tags.map(tag => <p className="tag">{tag}</p>)
+  const joinButton = link && (
+    <a href={link} className="join-button">
+      Saiba mais
+    </a>
+  )
   return (
     <li className="card">
-      <h3>{title}</h3>
-      <h4>{date}</h4>
-      <p>{description}</p>
-      <p>{forBegginers && 'Iniciantes são bem vindos.'}</p>
-      <p>Organizado por {organizer}</p>
+      <h1>{title}</h1>
+      <p>
+        <strong>{date}</strong>
+      </p>
+      <div className="card-content">
+        <p>{description}</p>
+        {joinButton}
+        <p>{forBegginers && 'Iniciantes são bem vindos.'}</p>
+        <p>Organizado por {organizer}</p>
+      </div>
       <div className="tag-container">{tagsElements}</div>
     </li>
   )
