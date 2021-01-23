f5cc7d4b2641106871df4a86f394fa82b634e4ba
Mark Kop <marcelokopmann@gmail.com>
2021-01-23 00:39:25 -0300

    refac: move static script tags to _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
-import Head from 'next/head'
 import NavBar from '../components/NavBar'
+import Head from 'next/head'
 
 function MyApp ({ Component, pageProps }) {
   return (
@@ -8,10 +8,6 @@ function MyApp ({ Component, pageProps }) {
       <Head>
         <title>Tarrafa Hackerclub</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
       </Head>
       <NavBar/>
       <Component {...pageProps} />
